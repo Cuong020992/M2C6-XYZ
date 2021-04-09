a4df2629b8040abecf7dae8f8601b9f3715b2e70
cuong0209992 <sandtimezone92@gmail.com>
2021-04-09 14:47:33 +0700

    M2C6

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Point2D point2D = new Point2D();
+        Point3D point3D = new Point3D();
+        System.out.println(point2D);
+        for(float i : point2D.getXY()){
+            System.out.println(i);
+        }
+
+        System.out.println(point3D);
+        for(float i : point3D.getXYZ()){
+            System.out.println(i);
+        }
+    }
+}
